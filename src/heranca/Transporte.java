dbdcd58b8bf94803d1d0e35262ce4100376acfc5
Julio Cesar Dellani Gabardo <julio.gabardo@alunos.sc.senac.br>
2022-09-05 21:06:58 -0300

    Heranca

---
+++ src/heranca/Transporte.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heranca;
+
+/**
+ *
+ * @author julio.gabardo
+ */
+public class Transporte {
+   private int capacidade;
+
+    public int getCapacidade() {//alt+insert geter e setter
+        return capacidade;
+    }
+   
+   public void SetCapacidade(int capacidade){
+       this.capacidade=capacidade;
+       
+   }
+   
+}
